4246d24276b4ef42210f1373e0a0835ef6f1cf36
JonathanClara <se3bclara@gmail.com>
2025-06-17 03:06:40 +0000

    terminada la lista de contactos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,63 @@
-import rigoImageUrl from "../assets/img/rigo-baby.jpg";
-import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import React, { useEffect } from "react";
+import useGlobalReducer from "../hooks/useGlobalReducer";
+import { Link } from "react-router-dom";
+import { Card } from "../components/Card";
 
-export const Home = () => {
+const Home = () => {
+    const { store, dispatch } = useGlobalReducer();
 
-  const {store, dispatch} =useGlobalReducer()
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-		</div>
-	);
-}; 
+    const fetchContacts = async () => {
+        try {
+            const response = await fetch("https://playground.4geeks.com/contact/agendas/Jonathan/contacts");
+            if (!response.ok) throw new Error("Error al obtener contactos");
+            const data = await response.json();
+            dispatch({ type: "SET_CONTACTS", payload: data.contacts });
+            return data;
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    }
+
+    useEffect(() => {
+        fetchContacts();
+    }, []);
+
+
+    if (store.contacts.length === 0) {
+        return (
+            <div>
+                <h2> No hay Contactos</h2>
+                <Link to="/addcontact">
+                    <button>Agregar Contacto</button>
+                </Link>
+            </div>
+        );
+    }
+    if (store.contacts.length > 0) {
+        return (
+            <div>
+                <div className="d-flex justify-content-between">
+                    <h2> Contactos </h2>
+                    <Link to="/addcontact">
+                        <button className="btn btn-secondary m-1">Agregar Contacto</button>
+                    </Link>
+                </div>
+
+                <div className="d-flex flex-wrap">
+                    {store.contacts.map((contact, index) => (
+                        <div className="d-flex" key={index}>
+                            <Card contact={contact} />
+                        </div>
+                    ))
+                    }
+                </div>
+
+
+            </div>
+        );
+    }
+
+};
+
+export default Home;
--- src/store.js
+++ src/store.js
@@ -1,32 +1,32 @@
-export const initialStore=()=>{
-  return{
-    message: null,
-    todos: [
-      {
-        id: 1,
-        title: "Make the bed",
-        background: null,
-      },
-      {
-        id: 2,
-        title: "Do my homework",
-        background: null,
-      }
-    ]
-  }
-}
+export const initialStore = {
+  contacts: []
+};
 
-export default function storeReducer(store, action = {}) {
-  switch(action.type){
-    case 'add_task':
-
-      const { id,  color } = action.payload
-
+export default function contactReducer(store, action = {}) {
+  switch (action.type) {
+    case "ADD_CONTACT":
       return {
         ...store,
-        todos: store.todos.map((todo) => (todo.id === id ? { ...todo, background: color } : todo))
+        contacts: [...store.contacts, action.payload]
+      };
+    case "REMOVE_CONTACT":
+      return {
+        ...store,
+        contacts: store.contacts.filter(contact => contact.id !== action.payload)
+      };
+    case "SET_CONTACTS":
+      return {
+        ...store,
+        contacts: action.payload
+      };
+      case "UPDATE_CONTACT":
+      return {
+        ...store,
+        contacts: store.contacts.map((contact, index) =>
+          index === action.payload.index ? { ...contact, ...action.payload.data } : contact
+        )
       };
     default:
-      throw Error('Unknown action.');
-  }    
+      throw new Error(`Unknown action type: ${action.type}`);
+  }
 }
